181cc698624052158f9d2fc01042f519f1a8852a
hemel ahmed <himelahmed1442@gmail.com>
2023-12-26 14:23:55 +0600

    Commit message describing your changes

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// $('.').slick({});
+
+$('.destination_part').slick({
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  centerMode: true,
+  centerPadding: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+$('.counter_plagin').counterUp({
+  delay: 10,
+  time: 5000,
+});
